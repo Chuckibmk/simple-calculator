d92a60f2756eeff801bfbcf88d5abec54d2b3365
Chuckibmk <tsartobnrys@gmail.com>
2024-07-16 21:44:07 +0100

    9:44

--- lib/main.dart
+++ lib/main.dart
@@ -127,15 +127,15 @@ class _HomeState extends State<Home> {
       // appBar: AppBar(title: Text('Bottom '))
       body: Column(
         children: [
-          if (_bannerAd != null)
-            Align(
-              alignment: Alignment.topCenter,
-              child: SizedBox(
-                width: _bannerAd!.size.width.toDouble(),
-                height: _bannerAd!.size.height.toDouble(),
-                child: AdWidget(ad: _bannerAd!),
-              ),
-            ),
+          // if (_bannerAd != null)
+          //   Align(
+          //     alignment: Alignment.topCenter,
+          //     child: SizedBox(
+          //       width: _bannerAd!.size.width.toDouble(),
+          //       height: _bannerAd!.size.height.toDouble(),
+          //       child: AdWidget(ad: _bannerAd!),
+          //     ),
+          //   ),
           PageView(
             controller: _controller,
             children: [
