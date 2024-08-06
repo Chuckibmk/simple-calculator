a511079b5569b5e856747e3f1d0a4804f1ee8006
Chuckibmk <tsartobnrys@gmail.com>
2024-08-06 22:11:13 +0100

    admob update

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 class Adhelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-9734967621956740/2431165448';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-9734967621956740/2431165448';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -13,21 +13,21 @@ class Adhelper {
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/1033173712';
+      return 'ca-app-pub-9734967621956740/9780413091';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/4411468910';
+      return 'ca-app-pub-9734967621956740/9780413091';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
   }
 
-  static String get rewardedAdUnitId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/5224354917';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/1712485313';
-    } else {
-      throw UnsupportedError('Unsupported platform');
-    }
-  }
+  // static String get rewardedAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return 'ca-app-pub-3940256099942544/5224354917';
+  //   } else if (Platform.isIOS) {
+  //     return 'ca-app-pub-3940256099942544/1712485313';
+  //   } else {
+  //     throw UnsupportedError('Unsupported platform');
+  //   }
+  // }
 }
